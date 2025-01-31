be57fea35fb0bb77bfa0eeebe37c19ce0b7de8ba
Maath-Lima <math3226@gmail.com>
2025-01-30 22:01:05 -0300

    [feat] Create stock func

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -2,9 +2,12 @@ package middleware
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"stocks-api/models"
 
 	"github.com/joho/godotenv"
 )
@@ -35,4 +38,39 @@ func CreateConnection() *sql.DB {
 
 	fmt.Println("Successufly connected to postgres")
 	return db
+}
+
+func CreateStock(w http.ResponseWriter, r *http.Request) {
+	var stock models.Stock
+
+	err := json.NewDecoder(r.Body).Decode(&stock)
+
+	if err != nil {
+		log.Fatal("Unable to decode the request body. %v", err)
+	}
+
+	insertID := insertStock(stock)
+
+	res := response{
+		ID:      insertID,
+		Message: "stock created successfully",
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
+
+func GetStock() {
+
+}
+
+func GetAllStock() {
+
+}
+
+func UpdateStock() {
+
+}
+
+func DeleteStock() {
+
 }
