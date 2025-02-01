f2276c815e5a59ae321efc96c5113223bdc88675
Maath-Lima <math3226@gmail.com>
2025-02-01 15:54:40 -0300

    [feat] Get, Get all and update stocks methods

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -8,7 +8,9 @@ import (
 	"net/http"
 	"os"
 	"stocks-api/models"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
@@ -46,7 +48,7 @@ func CreateStock(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&stock)
 
 	if err != nil {
-		log.Fatal("Unable to decode the request body. %v", err)
+		log.Fatalf("Unable to decode the request body. %v", err)
 	}
 
 	insertID := insertStock(stock)
@@ -59,16 +61,61 @@ func CreateStock(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(res)
 }
 
-func GetStock() {
+func GetStock(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
 
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Unavle to convert the string into int. %v", err)
+	}
+
+	stock, err := getStock(int64(id))
+
+	if err != nil {
+		log.Fatalf("Unable to get stock. %v", err)
+	}
+
+	json.NewEncoder(w).Encode(stock)
 }
 
-func GetAllStock() {
+func GetAllStock(w http.ResponseWriter, r *http.Request) {
+	stocks, err := getAllStock()
 
+	if err != nil {
+		log.Fatalf("Unable to get all the stcoks %v", err)
+	}
+
+	json.NewEncoder(w).Encode(stocks)
 }
 
-func UpdateStock() {
+func UpdateStock(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
 
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Unable to convert the string into int. %v", err)
+	}
+
+	var stock models.Stock
+
+	err = json.NewDecoder(r.Body).Decode(&stock)
+
+	if err != nil {
+		log.Fatalf("Unavble to decode the request body. %v", err)
+	}
+
+	updatedRows := updateStock(int64(id), stock)
+
+	msg := fmt.Sprintf("Stock updated successfully. Total rows/records affected %v", updatedRows)
+
+	res := response{
+		ID:      int64(id),
+		Message: msg,
+	}
+
+	json.NewEncoder(w).Encode(res)
 }
 
 func DeleteStock() {
